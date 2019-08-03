5b76825550f8c18804c70f6aaef07d3e276dff31
Arve Knudsen <arve.knudsen@gmail.com>
2019-08-03 18:19:44 +0200

    Develop example client

--- example-client/main.go
+++ example-client/main.go
@@ -9,11 +9,22 @@ import (
 )
 
 func main() {
-  _, err := pubsub.NewClient(context.Background(),
+  ctx := context.Background()
+  client, err := pubsub.NewClient(ctx,
     "experimental-berlin", option.WithCredentialsFile("keyfile.json"),
   )
   if err != nil {
     fmt.Fprintf(os.Stderr, "Failed to create pubsub client: %s\n", err)
     os.Exit(1)
   }
+
+  defer client.Close()
+
+  topic, err := client.CreateTopic(ctx, "imageProcessingResponses")
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "Failed to create topic: %s\n", err)
+    os.Exit(1)
+  }
+
+  fmt.Printf("Successfully created topic: %s\n", topic)
 }
