5a8bb17185d13fd2e9ba1b5a49541e0b1bf670dd
Arve Knudsen <arve.knudsen@gmail.com>
2019-11-05 21:32:18 +0100

    Fix formatting of example-client

--- example-client/main.go
+++ example-client/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "fmt"
-  "cloud.google.com/go/pubsub"
-  "google.golang.org/api/option"
-  "context"
-  "os"
+	"cloud.google.com/go/pubsub"
+	"context"
+	"fmt"
+	"google.golang.org/api/option"
+	"os"
 )
 
 func main() {
-  ctx := context.Background()
-  client, err := pubsub.NewClient(ctx,
-    "experimental-berlin", option.WithCredentialsFile("keyfile.json"),
-  )
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "Failed to create pubsub client: %s\n", err)
-    os.Exit(1)
-  }
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx,
+		"experimental-berlin", option.WithCredentialsFile("keyfile.json"),
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create pubsub client: %s\n", err)
+		os.Exit(1)
+	}
 
-  defer client.Close()
+	defer client.Close()
 
-  topic, err := client.CreateTopic(ctx, "imageProcessingResponses")
-  if err != nil {
-    fmt.Fprintf(os.Stderr, "Failed to create topic: %s\n", err)
-    os.Exit(1)
-  }
+	topic, err := client.CreateTopic(ctx, "imageProcessingResponses")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create topic: %s\n", err)
+		os.Exit(1)
+	}
 
-  fmt.Printf("Successfully created topic: %s\n", topic)
+	fmt.Printf("Successfully created topic: %s\n", topic)
 }
